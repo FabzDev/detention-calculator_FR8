e73d9a26c8904ccace57cfc4fee8a7dbf74d62af
fabztech <fabztech@outlook.com>
2022-08-21 09:45:21 -0500

    metodo

---
+++ metodo.js
@@ -0,0 +1,8 @@
+var autos = {
+  marca: "Renault",
+  modelo: "Sandero",
+  annio: 2011,
+  detalleAuto: function () {
+    console.log(`Vehiculo ${this.marca} ${this.modelo} ${this.annio}`);
+  },
+};
