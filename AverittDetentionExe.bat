b54d32b7db1a248574117c6852a11665c42c4778
Fabio <fabioescobarardila@hotmail.com>
2024-01-31 15:58:21 -0500

    Created: Averitt freight detention calculator

---
+++ AverittDetentionExe.bat
@@ -0,0 +1,1 @@
+python averitt_detention.py
---
+++ averitt_detention.py
@@ -0,0 +1,32 @@
+from datetime import datetime
+import math
+
+
+in_time_str = input('\nPlease enter the time in (HH:MM): ')
+out_time_str = input('Please enter the time out (HH:MM): ')
+free_time_str = input('Please enter the free time (in minutes): ')
+detention_percentage = input('Please enter the detention percentage: ')
+
+TIME_RANGE = 15
+DETENTION_CHARGE = 45
+
+def det_calculation():
+    global actual_detention
+    in_time = datetime.strptime(in_time_str, "%H:%M")
+    out_time = datetime.strptime(out_time_str, "%H:%M")
+
+    effective_time = (out_time - in_time).total_seconds() / 60 - int(free_time_str)
+
+    effective_fractions = math.ceil(effective_time / TIME_RANGE)
+
+    total_detention = effective_fractions * DETENTION_CHARGE
+
+    actual_detention = total_detention * float(detention_percentage) / 100
+
+    input(f"\n\t💸 Detention amount: ${actual_detention}")
+
+
+
+if __name__ == "__main__":
+    det_calculation()
+    
